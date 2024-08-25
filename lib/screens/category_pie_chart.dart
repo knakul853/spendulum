b54d97fc293cea3c0e35c655c72015af62d093dc
knakul_853 <nakul.bharti@gsat.us>
2024-08-25 17:56:06 +0530

    add card for charts

--- lib/screens/category_pie_chart.dart
+++ lib/screens/category_pie_chart.dart
@@ -9,13 +9,22 @@ class CategoryPieChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 1.8,
-      child: PieChart(
-        PieChartData(
-          sections: _generateSections(context, categoryTotals),
-          sectionsSpace: 0,
-          centerSpaceRadius: 25,
+    return Card(
+      elevation: 8, // Shadow effect
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12), // Rounded corners
+      ),
+      child: AspectRatio(
+        aspectRatio: 1.8,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0), // Padding inside the card
+          child: PieChart(
+            PieChartData(
+              sections: _generateSections(context, categoryTotals),
+              sectionsSpace: 0,
+              centerSpaceRadius: 25,
+            ),
+          ),
         ),
       ),
     );
@@ -29,9 +38,9 @@ class CategoryPieChart extends StatelessWidget {
         color: color,
         value: entry.value,
         title: '${entry.key}\n${entry.value.toStringAsFixed(2)}',
-        radius: 100,
+        radius: 70,
         titleStyle: TextStyle(
-          fontSize: 12,
+          fontSize: 8,
           fontWeight: FontWeight.bold,
           color: Theme.of(context).textTheme.bodyMedium?.color,
         ),
--- lib/screens/expense_chart.dart
+++ lib/screens/expense_chart.dart
@@ -9,103 +9,119 @@ class ExpenseChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.70,
-      child: LineChart(
-        LineChartData(
-          gridData: FlGridData(show: false),
-          titlesData: FlTitlesData(
-            bottomTitles: AxisTitles(
-              sideTitles: SideTitles(
-                showTitles: true,
-                getTitlesWidget: (value, meta) {
-                  if (value.toInt() >= 0 &&
-                      value.toInt() < monthlyData.length) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        DateFormat('MMM')
-                            .format(monthlyData[value.toInt()]['date']),
-                        style: TextStyle(
-                          color: Theme.of(context).textTheme.bodyMedium?.color,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 8,
-                        ),
-                      ),
-                    );
-                  }
-                  return const Text('');
-                },
-                reservedSize: 40,
-              ),
-            ),
-            leftTitles: AxisTitles(
-              sideTitles: SideTitles(
-                showTitles: true,
-                getTitlesWidget: (value, meta) {
-                  return Text(
-                    '\$${value.toInt()}',
-                    style: TextStyle(
-                      color: Theme.of(context).textTheme.bodyMedium?.color,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 8,
-                    ),
-                  );
-                },
-                reservedSize: 40,
-              ),
-            ),
-            topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-            rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).cardColor,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 8,
+            offset: Offset(0, 4),
           ),
-          borderData: FlBorderData(show: true),
-          minX: 0,
-          maxX: monthlyData.length - 1.0,
-          minY: 0,
-          maxY: monthlyData
-                  .map((e) => e['total'] as double)
-                  .reduce((a, b) => a > b ? a : b) *
-              1.2,
-          lineBarsData: [
-            LineChartBarData(
-              spots: monthlyData.asMap().entries.map((entry) {
-                return FlSpot(entry.key.toDouble(), entry.value['total']);
-              }).toList(),
-              isCurved: true,
-              color: Theme.of(context).primaryColor,
-              barWidth: 3,
-              isStrokeCapRound: true,
-              dotData: FlDotData(show: true),
-              belowBarData: BarAreaData(
-                show: true,
-                color: Theme.of(context).primaryColor.withOpacity(0.2),
+        ],
+      ),
+      padding: const EdgeInsets.all(16.0),
+      child: AspectRatio(
+        aspectRatio: 2.70,
+        child: LineChart(
+          LineChartData(
+            gridData: FlGridData(show: false),
+            titlesData: FlTitlesData(
+              bottomTitles: AxisTitles(
+                sideTitles: SideTitles(
+                  showTitles: true,
+                  getTitlesWidget: (value, meta) {
+                    if (value.toInt() >= 0 &&
+                        value.toInt() < monthlyData.length) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          DateFormat('MMM')
+                              .format(monthlyData[value.toInt()]['date']),
+                          style: TextStyle(
+                            color:
+                                Theme.of(context).textTheme.bodyMedium?.color,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 8,
+                          ),
+                        ),
+                      );
+                    }
+                    return const Text('');
+                  },
+                  reservedSize: 40,
+                ),
               ),
-            ),
-          ],
-          lineTouchData: LineTouchData(
-            enabled: true,
-            touchTooltipData: LineTouchTooltipData(
-              tooltipBgColor: Colors.blueGrey.withOpacity(0.8),
-              tooltipRoundedRadius: 8,
-              getTooltipItems: (List<LineBarSpot> touchedBarSpots) {
-                return touchedBarSpots.map((barSpot) {
-                  final flSpot = barSpot;
-                  if (flSpot.x.toInt() >= 0 &&
-                      flSpot.x.toInt() < monthlyData.length) {
-                    final data = monthlyData[flSpot.x.toInt()];
-                    return LineTooltipItem(
-                      '${DateFormat('MMMM yyyy').format(data['date'])}\n\$${data['total'].toStringAsFixed(2)}',
-                      const TextStyle(
-                        color: Colors.white,
+              leftTitles: AxisTitles(
+                sideTitles: SideTitles(
+                  showTitles: true,
+                  getTitlesWidget: (value, meta) {
+                    return Text(
+                      '\$${value.toInt()}',
+                      style: TextStyle(
+                        color: Theme.of(context).textTheme.bodyMedium?.color,
                         fontWeight: FontWeight.bold,
+                        fontSize: 8,
                       ),
                     );
-                  }
-                  return null;
-                }).toList();
-              },
+                  },
+                  reservedSize: 40,
+                ),
+              ),
+              topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+              rightTitles:
+                  AxisTitles(sideTitles: SideTitles(showTitles: false)),
             ),
-            handleBuiltInTouches: true,
+            borderData: FlBorderData(show: false), // Hide the border
+            minX: 0,
+            maxX: monthlyData.length - 1.0,
+            minY: 0,
+            maxY: monthlyData
+                    .map((e) => e['total'] as double)
+                    .reduce((a, b) => a > b ? a : b) *
+                1.2,
+            lineBarsData: [
+              LineChartBarData(
+                spots: monthlyData.asMap().entries.map((entry) {
+                  return FlSpot(entry.key.toDouble(), entry.value['total']);
+                }).toList(),
+                isCurved: true,
+                color: Theme.of(context).primaryColor,
+                barWidth: 3,
+                isStrokeCapRound: true,
+                dotData: FlDotData(show: true),
+                belowBarData: BarAreaData(
+                  show: true,
+                  color: Theme.of(context).primaryColor.withOpacity(0.2),
+                ),
+              ),
+            ],
+            lineTouchData: LineTouchData(
+              enabled: true,
+              touchTooltipData: LineTouchTooltipData(
+                tooltipBgColor: Colors.blueGrey.withOpacity(0.8),
+                tooltipRoundedRadius: 8,
+                getTooltipItems: (List<LineBarSpot> touchedBarSpots) {
+                  return touchedBarSpots.map((barSpot) {
+                    final flSpot = barSpot;
+                    if (flSpot.x.toInt() >= 0 &&
+                        flSpot.x.toInt() < monthlyData.length) {
+                      final data = monthlyData[flSpot.x.toInt()];
+                      return LineTooltipItem(
+                        '${DateFormat('MMMM yyyy').format(data['date'])}\n\$${data['total'].toStringAsFixed(2)}',
+                        const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      );
+                    }
+                    return null;
+                  }).toList();
+                },
+              ),
+              handleBuiltInTouches: true,
+            ),
           ),
         ),
       ),
