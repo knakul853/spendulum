ea8a9ff2554790823e00821afab84f88148f2cf2
knakul_853 <nakul.bharti@gsat.us>
2024-08-27 20:25:14 +0530

    improve account management ui

--- lib/screens/account_management_screen.dart
+++ lib/screens/account_management_screen.dart
@@ -21,7 +21,6 @@ class _AccountManagementScreenState extends State<AccountManagementScreen> {
   late Color _color;
   late String _currency;
 
-  // List of account types
   final List<String> _accountTypes = [
     'General',
     'Cash',
@@ -37,15 +36,14 @@ class _AccountManagementScreenState extends State<AccountManagementScreen> {
     'Other'
   ];
 
-  // List of currencies
   final List<String> _currencies = ['USD', 'EUR', 'GBP', 'JPY', 'INR', 'Other'];
 
   @override
   void initState() {
     super.initState();
-    _color = Colors.blue; // Default color
-    _accountType = _accountTypes[0]; // Default account type
-    _currency = _currencies[0]; // Default currency
+    _color = Colors.blue;
+    _accountType = _accountTypes[0];
+    _currency = _currencies[0];
   }
 
   void _submitForm() {
@@ -88,6 +86,21 @@ class _AccountManagementScreenState extends State<AccountManagementScreen> {
       ),
       body: Consumer<AccountProvider>(
         builder: (context, accountProvider, child) {
+          if (accountProvider.accounts.isEmpty) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('No accounts found.', style: TextStyle(fontSize: 18)),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () => _showAddAccountDialog(),
+                    child: Text('Create an Account'),
+                  ),
+                ],
+              ),
+            );
+          }
           return ListView.builder(
             itemCount: accountProvider.accounts.length,
             itemBuilder: (context, index) {
@@ -117,6 +130,7 @@ class _AccountManagementScreenState extends State<AccountManagementScreen> {
   void _showAddAccountDialog() {
     showDialog(
       context: context,
+      barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text('Add New Account'),
@@ -142,26 +156,81 @@ class _AccountManagementScreenState extends State<AccountManagementScreen> {
     );
   }
 
-  Widget _buildAccountForm() {
+  void _showEditAccountDialog(Account account) {
+    _name = account.name;
+    _accountNumber = account.accountNumber;
+    _accountType = account.accountType;
+    _balance = account.balance;
+    _color = account.color;
+    _currency = account.currency;
+
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Edit Account'),
+          content: SingleChildScrollView(
+            child: _buildAccountForm(isEditing: true),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            ElevatedButton(
+              child: Text('Save'),
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  _formKey.currentState!.save();
+                  final accountProvider =
+                      Provider.of<AccountProvider>(context, listen: false);
+                  accountProvider.updateAccount(
+                      account.id,
+                      _name,
+                      _accountNumber,
+                      _accountType,
+                      _balance,
+                      _color,
+                      _currency);
+                  Navigator.of(context).pop();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Account updated successfully')),
+                  );
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildAccountForm({bool isEditing = false}) {
     return Form(
       key: _formKey,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          _buildTextFormField('Account Name', (value) => _name = value!),
+          _buildTextFormField('Account Name', (value) => _name = value!,
+              initialValue: isEditing ? _name : null),
           SizedBox(height: 16),
           _buildTextFormField(
-              'Account Number', (value) => _accountNumber = value!),
+              'Account Number', (value) => _accountNumber = value!,
+              initialValue: isEditing ? _accountNumber : null),
           SizedBox(height: 16),
           _buildDropdownField(
-              'Account Type', _accountTypes, (value) => _accountType = value!),
+              'Account Type', _accountTypes, (value) => _accountType = value!,
+              initialValue: _accountType),
           SizedBox(height: 16),
           _buildTextFormField(
-              'Initial Balance', (value) => _balance = double.parse(value!),
-              keyboardType: TextInputType.number),
+              'Balance', (value) => _balance = double.parse(value!),
+              keyboardType: TextInputType.number,
+              initialValue: isEditing ? _balance.toString() : null),
           SizedBox(height: 16),
           _buildDropdownField(
-              'Currency', _currencies, (value) => _currency = value!),
+              'Currency', _currencies, (value) => _currency = value!,
+              initialValue: _currency),
           SizedBox(height: 16),
           _buildColorPicker(),
         ],
@@ -170,12 +239,13 @@ class _AccountManagementScreenState extends State<AccountManagementScreen> {
   }
 
   Widget _buildTextFormField(String label, Function(String?) onSaved,
-      {TextInputType? keyboardType}) {
+      {TextInputType? keyboardType, String? initialValue}) {
     return TextFormField(
       decoration: InputDecoration(
         labelText: label,
         border: OutlineInputBorder(),
       ),
+      initialValue: initialValue,
       keyboardType: keyboardType,
       validator: (value) => value!.isEmpty ? 'Please enter $label' : null,
       onSaved: onSaved,
@@ -183,13 +253,14 @@ class _AccountManagementScreenState extends State<AccountManagementScreen> {
   }
 
   Widget _buildDropdownField(
-      String label, List<String> items, Function(String?) onChanged) {
+      String label, List<String> items, Function(String?) onChanged,
+      {required String initialValue}) {
     return DropdownButtonFormField<String>(
       decoration: InputDecoration(
         labelText: label,
         border: OutlineInputBorder(),
       ),
-      value: items[0],
+      value: initialValue,
       items: items.map((String value) {
         return DropdownMenuItem<String>(
           value: value,
@@ -224,17 +295,19 @@ class _AccountManagementScreenState extends State<AccountManagementScreen> {
               pickerColor: _color,
               onColorChanged: (Color color) {
                 setState(() => _color = color);
-                Navigator.of(context).pop();
               },
             ),
           ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
         );
       },
     );
-  }
-
-  void _showEditAccountDialog(Account account) {
-    // Implement edit account dialog similar to add account dialog
-    // Pre-fill the form with existing account details
   }
 }
