98449037d60f3e4ed0d19b09e10995afe52ec236
knakul_853 <nakul.bharti@gsat.us>
2024-10-19 15:58:26 +0530

    added app colors

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF007BFF);
+  static const Color secondary = Color(0xFF6C757D);
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color text = Color(0xFF000000);
+  static const Color error = Color(0xFFDC3545);
+}
